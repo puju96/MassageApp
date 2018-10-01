324fcd4f7cc1fdbb2b0afa3585a496d428aea7c9
Admin <admin@Admins-Mac-2.local>
2018-10-01 16:38:22 +0530

    project created

---
+++ MassageApp/msgApp/msgApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  msgApp
+//
+//  Created by Admin on 09/07/1940 Saka.
+//  Copyright © 1940 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
